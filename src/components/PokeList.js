98909134195402698ebbd59a20ac4acd5564eec7
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-01 12:51:05 +0200

    add types from api

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -2,17 +2,14 @@ import React from 'react';
 import Pokemon from './Pokemon';
 import '../stylesheets/PokeList.scss';
 
-//AQUI LE TENGO QUE PASAR pokemonData
-const PokeList = (props) => {
-  // console.log(props);
-  const renderPokemon = props.pokemons.map((pokemon) => {
+const PokeList = ({ pokemons, handlePokemon }) => {
+  const renderPokemon = pokemons.map((pokemon) => {
     return (
       <li key={pokemon.id.toString()}>
-        <Pokemon pokemon={pokemon} handlePokemon={props.handlePokemon} />
+        <Pokemon pokemon={pokemon} handlePokemon={handlePokemon} />
       </li>
     );
   });
-
   return (
     <div className="pokeContainer">
       <ul className="pokeList">{renderPokemon}</ul>
@@ -22,4 +19,3 @@ const PokeList = (props) => {
 
 export default PokeList;
 
-// handlePokemon={props.handlePokemon} es la unión del lifting entre App y Pokemon
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import '../stylesheets/Pokemon.scss';
 
-const Pokemon = (props) => {
-  //LE METEMOS UN EVENTO
+const Pokemon = ({ pokemon, handlePokemon }) => {
   const handleClick = (ev) => {
-    props.handlePokemon(props.pokemon.id);
+    handlePokemon(pokemon.id);
   };
 
-  // const pokeTypes = props.pokemon.types.map((type, index) => {
-  //   return (
-  //     <li key={index} className={`poke__type poke__type--${type}`}>
-  //       {type}
-  //     </li>
-  //   );
-  // });
+  const pokeTypes = pokemon.types.map((type, index) => {
+    return (
+      <li key={index} className={`poke__type poke__type--${type}`}>
+        {type.type.name}
+      </li>
+    );
+  });
 
   return (
-    <article className="pokeCard" id={props.pokemon.id} onClick={handleClick}>
-      {/* <img src={props.pokemon.url} alt={props.pokemon.name} /> */}
-      <img src={props.pokemon.sprites.front_default} alt={props.pokemon.name} />
+    <article className="pokeCard" id={pokemon.id} onClick={handleClick}>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
       <div className="pokeDescription">
-        <h2 className="pokeTitle"> {props.pokemon.name} </h2>
-        {/* <ul className="pokeTypes">{pokeTypes}</ul> */}
+        <h2 className="pokeTitle"> {pokemon.name} </h2>
+        <ul className="pokeTypes">{pokeTypes}</ul>
       </div>
     </article>
   );
