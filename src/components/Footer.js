3faa99a3f6ad8daee31d304733dcba84870991e8
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-05 13:38:26 +0200

    add new footer with icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,54 @@
-import React from 'react';
 import '../stylesheets/Footer.scss';
 
 const Footer = () => {
   return (
-    <footer className="containerFooter">
-      <a href="https://www.linkedin.com/in/laura-portillo-rodríguez" target="_blank" className="containerFooter__name">
-        <small>By Laura Portillo Rodríguez</small>
-      </a>
-      <small>©2021</small>
+    <footer>
+      <ul className="footer">
+        <li className="footer__item">
+          <a
+            className="footer__item--icon"
+            href="https://www.linkedin.com/in/laura-portillo-rodr%C3%ADguez-21965a86/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="linkedin"
+          >
+            <i className="fa fa-linkedin" alt="linkedin icon"></i>
+          </a>
+        </li>
+        <li className="footer__item">
+          <a
+            className="footer__item--icon"
+            href="https://github.com/lauraportillo"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="github"
+          >
+            <i className="fa fa-github-alt" alt="github icon"></i>
+          </a>
+        </li>
+        <li className="footer__item">
+          <a
+            className="footer__item--icon"
+            href="https://twitter.com/LauraPo02860847"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="twitter"
+          >
+            <i className="fa fa-twitter" alt="twitter icon"></i>
+          </a>
+        </li>
+        <li className="footer__item">
+          <a
+            className="footer__item--icon"
+            href="mailto:lauraportillo.ark@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="email"
+          >
+            <i className="fa fa-envelope" alt="envelope icon"></i>
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
