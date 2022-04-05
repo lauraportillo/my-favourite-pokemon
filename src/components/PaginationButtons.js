4f67f2f5aa998940376ebd312cec9a28f6909962
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-05 11:39:45 +0200

    add style to previous and next button

--- src/components/PaginationButtons.js
+++ src/components/PaginationButtons.js
@@ -10,11 +10,21 @@ const PaginationButtons = (props) => {
     };
 
     return (
-        <section>
-            <button onClick={prev}>previous</button>
-            <button onClick={next}>next</button>
-        </section>
+        <nav >
+            <ul className="pagination">
+                <li key="left" className="pagination__item" onClick={prev}>
+                    <i className="fas fa-angle-double-left"></i>
+                    <p className="pagination__item--left">Previous</p>
+                </li>
+                <li className="pagination__item2">|</li>
+                <li key="right" className="pagination__item" onClick={next}>
+                    <p className="pagination__item--right">Next</p>
+                    <i className="fas fa-angle-double-right"></i>
+                </li>
+            </ul>
+        </nav>
     );
 };
 
 export default PaginationButtons;
+
