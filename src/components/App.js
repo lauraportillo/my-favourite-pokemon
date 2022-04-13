bef2d079c21c1e719a6ff688053857fc92907413
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-13 10:14:56 +0200

    delete files and comments

--- src/components/App.js
+++ src/components/App.js
@@ -21,54 +21,11 @@ const App = () => {
   // ERROR: TENEMOS QUE PODER ENCONTRAR LOS NOMBRES DE TODAS LAS PÁGINAS!!!!!
 
   const [pokemonData, setPokemonData] = useState([]);
-  const [nextUrl, setNextUrl] = useState('');
-  const [prevUrl, setPrevUrl] = useState('');
-  const [loading, setLoading] = useState(true);
-  const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
   const [fav, setFav] = useState({});
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     let response = await getAllPokemon(initialUrl);
-  //     setNextUrl(response.next);
-  //     setPrevUrl(response.previous);
-  //     await loadingPokemon(response.results);
-  //     setLoading(false);
-  //   }
-  //   fetchData();
 
-  // }, []);
-
-  // const next = async () => {
-  //   setLoading(true);
-  //   let data = await getAllPokemon(nextUrl);
-  //   await loadingPokemon(data.results);
-  //   setNextUrl(data.next);
-  //   setPrevUrl(data.previous);
-  //   setLoading(false);
-  // }
-
-  // const prev = async () => {
-  //   if (!prevUrl) return;
-  //   setLoading(true);
-  //   let data = await getAllPokemon(prevUrl);
-  //   await loadingPokemon(data.results);
-  //   setNextUrl(data.next);
-  //   setPrevUrl(data.previous);
-  //   setLoading(false);
-  // }
-
-  // const loadingPokemon = async data => {
-  //   let pokeData = await Promise.all(
-  //     data.map(async pokemon => {
-  //       let pokemonRecord = await getPokemon(pokemon.url);
-  //       return pokemonRecord;
-  //     })
-  //   );
-  //   setPokemonData(pokeData);
-  // }
 
 
   //definición de la función que maneja los cambios en los inputs e indentifica en qué input se está realizando el cambio.
