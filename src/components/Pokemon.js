21b9377fc833a39053c2276b03985eaabc6e6226
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-19 11:49:20 +0200

    add conditional if pokemon name is long

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -14,13 +14,28 @@ const Pokemon = ({ pokemon, handlePokemon }) => {
   // });
 
   // HACER UN CONDICIONAL PARA LOS TAMAÑOS DE LETRA
+  // REVISAR Y HACER RENDERIZADO CONDICIONAL REACT************************************************************
+  const renderPokeName = () => {
+    if ((pokemon.name.length <= 10)) {
+      return (
+        <h2 className="pokeTitle"> {pokemon?.name} </h2>
+      )
+
+    } else {
+      return (
+        <h2 className="pokeTitleSmaller"> {pokemon?.name} </h2>
+      )
+
+    }
+  }
 
   return (
     <article className="pokeCard" id={pokemon?.id} onClick={handleClick}>
       <img src={pokemon?.pic} alt={pokemon?.name} />
       <div className="pokeDescription">
-        <h2 className="pokeTitle"> {pokemon?.name} </h2>
-        <p className="pokeSubtitle">{pokemon?.id}</p>
+        {/* <h2 className="pokeTitle"> {pokemon?.name} </h2> */}
+        {renderPokeName()}
+        {<p className="pokeSubtitle">{pokemon?.id}</p>}
         {/* <ul className="pokeTypes">{pokeTypes}</ul> */}
       </div>
     </article>
