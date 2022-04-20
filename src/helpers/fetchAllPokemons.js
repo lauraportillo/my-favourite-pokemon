1f492e4cd459316ea57dba1e779b49798fb912e1
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-20 10:23:12 +0200

    update changes

--- src/helpers/fetchAllPokemons.js
+++ src/helpers/fetchAllPokemons.js
@@ -17,5 +17,22 @@ export const fetchAllPokemons = async () => {
     });
 }
 
+
 // fetchAllPokemons returns the transformed array
 
+
+// export const fetchAllPokemons = async () => {
+//     const resp = await pokemonApi.get('/pokemon?limit=1500');
+//     console.log(resp);
+
+//     return resp.data.results.map((poke) => {
+//         const pokeArr = poke.url.split('/');
+//         const id = pokeArr[6];
+//         // TENDRIA QUE HACER AQUI OTRA LLAMADA A LA API PARA QUE ME DE TODA LA INFO DE CADA POKEMON, PERO NO SÉ CÓMO HACERLO
+//         const fetchPokemonUrl = async () => {
+//             const respPokemon = await pokemonApi.get(`/pokemon${id}/`);
+//         }
+//         return {
+//         };
+//     });
+// }
