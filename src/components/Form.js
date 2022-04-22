e4be12aaab96b6d53db1e2e34b2e9fbb7212cf4a
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-22 10:25:51 +0200

    add print btn

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,9 @@
 import Input from './Input';
 import FilterByName from './FilterByName';
 import ResetButton from './ResetButton';
+import PrintButton from './PrintButton';
 import '../stylesheets/Form.scss';
+
 
 const Form = (props) => {
   const handleForm = (ev) => {
@@ -12,6 +14,7 @@ const Form = (props) => {
       <Input handleName={props.handleName} username={props.username} />
       <FilterByName handleFilter={props.handleFilter} name={props.name} />
       <ResetButton handleReset={props.handleReset} />
+      <PrintButton />
     </form>
   );
 };
---
+++ src/components/PrintButton.js
@@ -0,0 +1,9 @@
+import '../stylesheets/Form.scss';
+
+const PrintButton = () => {
+    return (
+        <button className="form__print">Print</button>
+    )
+};
+
+export default PrintButton;
