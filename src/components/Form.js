56632ab1b39cdd9c694af5301ebb0c43182933dd
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-22 10:33:22 +0200

    adjust btns style

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,10 +11,14 @@ const Form = (props) => {
   };
   return (
     <form onSubmit={handleForm} className="form">
+      <div className="form__div">
       <Input handleName={props.handleName} username={props.username} />
       <FilterByName handleFilter={props.handleFilter} name={props.name} />
+      </div>
+      <div className="form__div">
       <ResetButton handleReset={props.handleReset} />
       <PrintButton />
+      </div>
     </form>
   );
 };
