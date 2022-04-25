a46913eaa6541d00e2ac9a97bf4b5e0ef0123c2e
Laura Portillo <lauraportillo.ark@gmail.com>
2022-04-25 15:52:00 +0200

    clean console log

--- src/helpers/fetchAllPokemons.js
+++ src/helpers/fetchAllPokemons.js
@@ -3,7 +3,6 @@ import { pokemonApi } from "../services/pokemonApi"
 
 export const fetchAllPokemons = async () => {
     const resp = await pokemonApi.get('/pokemon?limit=1500');
-    console.log(resp);
 
     return resp.data.results.map((poke) => {
         const pokeArr = poke.url.split('/');
